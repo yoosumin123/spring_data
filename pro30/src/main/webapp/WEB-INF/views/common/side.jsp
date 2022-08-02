fd035d2e6fd2400c783ccc7f6d7e7c758cdc8778
tnals4211 <tnals4211@naver.com>
2022-08-02 18:02:57 +0900

    0802 spring

--- pro30/src/main/webapp/WEB-INF/views/common/side.jsp
+++ pro30/src/main/webapp/WEB-INF/views/common/side.jsp
@@ -20,18 +20,19 @@
   <title>사이드 메뉴</title>
 </head>
 <body>
-	<h1>사이드 메뉴</h1>
-	<h1>
+	<h1>사이드 메뉴</h1>  
+	
+	<!-- <h1>
 		<a href="#"  class="no-underline">회원관리</a><br>
 	    <a href="#"  class="no-underline">게시판관리</a><br>
 	    <a href="#"  class="no-underline">상품관리</a><br>
-    </h1>
-	<%-- 
-	<h1>
+    </h1> -->
+	 
+	<h1> <!-- 링크 시킨것 -->
 		<a href="${contextPath}/member/listMembers.do"  class="no-underline">회원관리</a><br>
 		<a href="${contextPath}/board/listArticles.do"  class="no-underline">게시판관리</a><br>
 		<a href="#"  class="no-underline">상품관리</a><br>
 	</h1>
-	 --%>
+	
 </body>
 </html>
---
+++ project30/src/main/java/com/myspring/pro30/member/controller/MemberControllerImpl.java
@@ -0,0 +1,134 @@
+package com.myspring.pro30.member.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.myspring.pro30.member.service.MemberService;
+import com.myspring.pro30.member.vo.MemberVO;
+
+
+
+@Controller("memberController")
+@EnableAspectJAutoProxy
+public class MemberControllerImpl   implements MemberController {
+//	private static final Logger logger = LoggerFactory.getLogger(MemberControllerImpl.class);
+	@Autowired
+	private MemberService memberService;
+	@Autowired
+	MemberVO memberVO ;
+	
+	// pro30/main.do 로 요청 시 메인 페이지를 보여준다.
+	@RequestMapping(value = { "/","/main.do"}, method = RequestMethod.GET)
+	private ModelAndView main(HttpServletRequest request, HttpServletResponse response) {
+		String viewName = (String)request.getAttribute("viewName");
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName(viewName);
+		return mav;  }
+	
+	@Override
+	@RequestMapping(value="/member/listMembers.do" ,method = RequestMethod.GET)
+	public ModelAndView listMembers(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		String viewName = getViewName(request); //request를 가져와서 getViewName으로 잘랐는데
+		String viewName = (String)request.getAttribute("viewName"); //viewName을 가져와서 인터셉터에서 자른다
+		//System.out.println("viewName: " +viewName);
+//		logger.info("info 레벨 :viewName = "+ viewName);
+//		logger.debug("debug 레벨 :viewName = "+ viewName); // 레벨에 따라서 실행
+		List membersList = memberService.listMembers();
+		ModelAndView mav = new ModelAndView(viewName);
+		mav.addObject("membersList", membersList);
+		return mav;
+	}
+
+	@Override
+	@RequestMapping(value="/member/addMember.do" ,method = RequestMethod.POST)
+	public ModelAndView addMember(@ModelAttribute("member") MemberVO member,
+			                  HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("utf-8");
+		int result = 0;
+		result = memberService.addMember(member);
+		ModelAndView mav = new ModelAndView("redirect:/member/listMembers.do");
+		return mav;
+	}
+	
+	@Override
+	@RequestMapping(value="/member/removeMember.do" ,method = RequestMethod.GET)
+	public ModelAndView removeMember(@RequestParam("id") String id, 
+			           HttpServletRequest request, HttpServletResponse response) throws Exception{
+		request.setCharacterEncoding("utf-8");
+		memberService.removeMember(id);
+		ModelAndView mav = new ModelAndView("redirect:/member/listMembers.do");
+		return mav;
+	}
+	/*
+	@RequestMapping(value = { "/member/loginForm.do", "/member/memberForm.do" }, method =  RequestMethod.GET)
+	@RequestMapping(value = "/member/*Form.do", method =  RequestMethod.GET)
+	public ModelAndView form(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String viewName = getViewName(request);
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName(viewName);
+		return mav;
+	}
+	*/
+	
+	@Override
+	@RequestMapping(value = "/member/login.do", method = RequestMethod.POST)
+	public ModelAndView login(@ModelAttribute("member") MemberVO member,
+				              RedirectAttributes rAttr,
+		                       HttpServletRequest request, HttpServletResponse response) throws Exception {
+	ModelAndView mav = new ModelAndView();
+	memberVO = memberService.login(member);
+	if(memberVO != null) {
+		    HttpSession session = request.getSession();
+		    session.setAttribute("member", memberVO); //로그인 ->setAttribute
+		    session.setAttribute("isLogOn", true); // set -> 페이지마다 저장되니까 / session
+		    mav.setViewName("redirect:/member/listMembers.do");
+	}else {
+		    rAttr.addAttribute("result","loginFailed");
+		    mav.setViewName("redirect:/member/loginForm.do");
+	}
+	return mav;
+	}
+
+	@Override
+	@RequestMapping(value = "/member/logout.do", method =  RequestMethod.GET)
+	public ModelAndView logout(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = request.getSession();
+		session.removeAttribute("member"); // remove -> 로그아웃 -- member에 회원정보
+		session.removeAttribute("isLogOn"); // 로그인상태인지 로그아웃상태인지
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("redirect:/member/listMembers.do");
+		return mav;
+	}	
+
+	@RequestMapping(value = "/member/*Form.do", method =  RequestMethod.GET)
+	private ModelAndView form(@RequestParam(value= "result", required=false) String result,
+						       HttpServletRequest request, 
+						       HttpServletResponse response) throws Exception {
+//		String viewName = getViewName(request);
+		String viewName = (String)request.getAttribute("viewName");
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("result",result);
+		mav.setViewName(viewName);
+		return mav;
+	}
+	
+
+
+
+
+}
