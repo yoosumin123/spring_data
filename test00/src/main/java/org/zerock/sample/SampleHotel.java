57bb769303396b2fb8b3ada7baaaaa476150e7a4
tnals4211 <tnals4211@naver.com>
2022-08-23 18:46:43 +0900

    test

--- test00/src/main/java/org/zerock/sample/SampleHotel.java
+++ test00/src/main/java/org/zerock/sample/SampleHotel.java
@@ -2,18 +2,21 @@ package org.zerock.sample;
 
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
-@Component
-@ToString
-@Getter
+@Component // 빈에 등록 하는 역할
+@ToString // tostring 
+@Getter // getter  => @data -> get/set 둘다 만들어버림 
+@RequiredArgsConstructor // @NonNull -> 붙은 변수에 대해서만 생성자 만든다.
+//@AllArgsConstructor // 생성자를 만들어줌
 public class SampleHotel {
 	
+	@NonNull // 
 	private Chef chef;
 	
-	public SampleHotel(Chef chef) {
-		this.chef = chef;
-	}
-
+	
 }
